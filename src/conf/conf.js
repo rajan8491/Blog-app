079bcebb695e9f8eb9f652f014c40f84c96c2aca
Rajan Yadav <rajankumaryadav409@gmail.com>
2025-07-13 01:58:57 +0530

    add configuration for Appwrite integration and update .gitignore

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwrite_url: String(import.meta.env.VITE_APPWRITE_URL),
+    appwrite_database_id: String(import.meta.env.VITE_DATABASE_ID),
+    appwrite_collection_id: String(import.meta.env.VITE_COLLECTION_ID),
+    appwrite_bucket_id: String(import.meta.env.VITE_BUCKET_ID),
+}
+
+export default conf
